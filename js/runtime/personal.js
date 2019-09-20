718e2786628f97b3ffea4a0d6474b7ce4d1daf67
ihboy <ly5569dhb@sina.cn>
2019-09-20 17:59:33 +0800

    FF

--- js/runtime/personal.js
+++ js/runtime/personal.js
@@ -6,24 +6,52 @@ const screenHeight = window.innerHeight
 const BG_IMG_SRC = 'images/bg_03.png'
 const BG_WIDTH = 290
 const BG_HEIGHT = 440
+const sX = (screenWidth - BG_WIDTH) / 2
+const sY = (screenHeight - BG_HEIGHT) / 2
 
 export default class Personal extends Sprite {
   constructor() {
-    let sX = (screenWidth - BG_WIDTH) / 2
-    let sY = (screenHeight - BG_HEIGHT) / 2
     super(BG_IMG_SRC, BG_WIDTH, BG_HEIGHT, sX, sY)
     console.log(sX, sY)
     this.visible = false
 
     this.closeBtn = {
-      startX: sX + 77,
-      startY: sY + 360,
-      endX: sX + 77 + 16,
-      endY: sY + 360 + 25
+      startX: screenWidth - sX - 35,
+      startY: sY + 35,
+      endX: screenWidth - sX,
+      endY: sY + 35 + 25
 
     }
   }
 
+  fillText (data) {
+    if(!data.length){
+      return 
+    }
 
+    ctx.fillStyle = "#ffffff"
+    ctx.font = "20px Arial"
+    ctx.textAlign = "center"
+    ctx.textBaseline = "middle"
+    let sL = 0;
+    let sT = 0;
+    let src = "images/enemy/"
+
+    for (var i=0;i< data.length; i++) {
+      let item = data[i]
+      ctx.drawImage(
+        src+'1.png',
+        sL, sT, 60, 60
+      )
+
+      ctx.fillText(
+        '山药',
+        screenWidth / 2 - 40,
+        screenHeight / 2 - 100 + 175
+      )
+    }
+
+
+  }
 
 }
